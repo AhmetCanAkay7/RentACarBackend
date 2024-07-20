3c3103f35de1eecfa3266e4af8f98192ba2745fd
AhmetCanAkay7 <142002072+AhmetCanAkay7@users.noreply.github.com>
2024-07-20 12:56:30 +0300

    ReCapProject

---
+++ ReCapProject/ConsoleUI/Program.cs
@@ -0,0 +1,38 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemoryCarDal;
+using Entities.Concrete;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            InMemoryCarDal inMemoryCarDal = new InMemoryCarDal();
+            CarManager carManager = new CarManager(inMemoryCarDal);
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+
+            
+            Console.WriteLine("-------------------------Hadi bir araç güncelleyelim ---------------------------");
+            Car carToUpdate = carManager.GetbyId(2);
+            carToUpdate.Description = "Audi A5 Sportback";
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+
+
+
+            Console.ReadLine();
+
+            
+        }
+
+    }
+}
